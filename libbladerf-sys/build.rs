dd16bc445780c9f5d3b312c1102e232430ad9693
Troy Neubauer <troyneubauer@gmail.com>
2024-11-30 07:57:02 -0800

    remove remaining android stuff

--- libbladerf-sys/build.rs
+++ libbladerf-sys/build.rs
@@ -14,40 +14,6 @@ fn main() -> anyhow::Result<()> {
         .header("wrapper.h")
         .allowlist_item("(bladerf|BLADERF).*");
 
-    let target = env::var("TARGET").expect("TARGET not set");
-    // Handle android oddities
-    if target.contains("android") {
-        let ndk = env::var("ANDROID_NDK_HOME")
-            .expect("ANDROID_NDK_HOME not set. This is required to build for android");
-
-        let prebuilt = format!("{ndk}/toolchains/llvm/prebuilt/linux-x86_64");
-        let sysroot = format!("{prebuilt}/sysroot");
-        println!("Adding sysroot path: {sysroot}");
-        let clang_base = format!("{prebuilt}/lib64/clang");
-
-        let clang_version = std::fs::read_dir(&clang_base)
-            .with_context(|| format!("Failed to open directory {clang_base}"))?
-            .next()
-            .with_context(|| format!("No enteries in {clang_base}"))?
-            .with_context(|| format!("Failed to read dir: {clang_base}"))?
-            .file_name();
-
-        let clang_version = clang_version
-            .to_str()
-            .to_owned()
-            .expect("Clang version name not utf8");
-        println!("Found clang version {clang_version} in ndk: {clang_base}");
-
-        builder = builder.clang_args(&[
-            format!("--target={target}"),
-            format!("--sysroot={sysroot}"),
-            format!("-I{sysroot}/usr/include"),
-            format!("-I{clang_base}/{clang_version}/include"),
-            format!("-I{sysroot}/usr/include/{target}"),
-            "-v".to_string(),
-        ]);
-    }
-
     if let Ok(path) = std::env::var("BLADERF_INCLUDE_PATH") {
         println!("Adding explicit blade rf include path: {path}");
         builder = builder.clang_arg(format!("-I{path}"));
