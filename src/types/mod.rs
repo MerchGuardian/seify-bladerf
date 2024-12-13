094d6bb8d6a754b4091841b6e2b17bc761a38f79
Erik Fong <quantumerikfong@gmail.com>
2024-12-12 20:35:11 -0500

    Only export the types, not the modules

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,62 +1,62 @@
-pub mod module_config;
+mod module_config;
 pub use module_config::ModuleConfig;
 
-pub mod version;
+mod version;
 pub use version::Version;
 
-pub mod log_level;
+mod log_level;
 pub use log_level::LogLevel;
 
-pub mod rational_rate;
+mod rational_rate;
 pub use rational_rate::RationalRate;
 
-pub mod backend;
+mod backend;
 pub use backend::Backend;
 
-pub mod dev_info;
+mod dev_info;
 pub use dev_info::DevInfo;
 
-pub mod config;
+mod config;
 pub use config::Config;
 
-pub mod channel;
+mod channel;
 pub use channel::Channel;
 
-pub mod metadata;
+mod metadata;
 pub use metadata::Metadata;
 
-pub mod direction;
+mod direction;
 pub use direction::Direction;
 
-pub mod loopback;
+mod loopback;
 pub use loopback::{Loopback, LoopbackModeInfo};
 
-pub mod format;
+mod format;
 pub use format::{Format, SampleFormat};
 
-pub mod sampling;
+mod sampling;
 pub use sampling::Sampling;
 
-pub mod rx_mux;
+mod rx_mux;
 pub use rx_mux::RxMux;
 
-pub mod lpf_mode;
+mod lpf_mode;
 pub use lpf_mode::LPFMode;
 
-pub mod quick_tune;
+mod quick_tune;
 pub use quick_tune::QuickTune;
 
-pub mod tuning_mode;
+mod tuning_mode;
 pub use tuning_mode::TuningMode;
 
-pub mod gain;
+mod gain;
 pub use gain::{Gain, GainMode, GainModeInfo};
 
-pub mod range;
+mod range;
 pub use range::Range;
 
-pub mod correction;
+mod correction;
 pub use correction::{Correction, CorrectionValue};
 
-pub mod trigger;
+mod trigger;
 pub use trigger::{Trigger, TriggerRole, TriggerSignal};
