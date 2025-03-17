4b802986d9afcdb58ed564c0c17bd32a413cfd47
Erik Fong <quantumerikfong@gmail.com>
2025-03-16 22:48:31 -0400

    Documentation for `DeviceSpeed`

--- src/types/device_speed.rs
+++ src/types/device_speed.rs
@@ -4,11 +4,20 @@ use strum::FromRepr;
 
 use crate::{sys::*, Error, Result};
 
+/// The [USB Device Speed](https://en.wikipedia.org/wiki/USB_hardware#Connectors) that the BladeRF will operate at.
+///
+/// Speeds not listed are not supported.
+///
+/// Relevant `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/group___f_n___i_n_f_o.html#ga9a3716d6cf5a1c25da8325fa245e92f9>
 #[derive(Copy, Clone, Debug, FromRepr, PartialEq, Eq)]
 #[repr(u32)]
 pub enum DeviceSpeed {
+    /// The Device Speed is unknown
+    // TODO figure out when this can show up.
     Unknown = bladerf_dev_speed_BLADERF_DEVICE_SPEED_UNKNOWN as u32,
+    /// [USB High Speed](https://en.wikipedia.org/wiki/USB_Hi-Speed)
     High = bladerf_dev_speed_BLADERF_DEVICE_SPEED_HIGH as u32,
+    /// [USB SuperSpeed](https://en.wikipedia.org/wiki/USB_SuperSpeed)
     Super = bladerf_dev_speed_BLADERF_DEVICE_SPEED_SUPER as u32,
 }
 
