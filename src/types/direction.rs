af89808d8f475f4ac40fd4c0f8631474186914e0
Erik Fong <quantumerikfong@gmail.com>
2025-03-16 22:50:31 -0400

    Documentation for `Direction`

--- src/types/direction.rs
+++ src/types/direction.rs
@@ -4,11 +4,15 @@ use strum::FromRepr;
 
 use crate::{sys::*, Error, Result};
 
-/// Direction Enum
+/// Specifies the direction of a stream.
+///
+/// Relevant `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/group___f_n___c_h_a_n_n_e_l.html#gaeea96995bb88c2f2d4fa7da5d30a1894>
 #[derive(Copy, Clone, Debug, FromRepr, PartialEq, Eq)]
 #[repr(u32)]
 pub enum Direction {
+    /// Recieve direction
     RX = bladerf_direction_BLADERF_RX as u32,
+    /// Transmit direction
     TX = bladerf_direction_BLADERF_TX as u32,
 }
 
