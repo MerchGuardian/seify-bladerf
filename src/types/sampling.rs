b1ad739f243d2310a1b72443fda104bb044014b0
Erik Fong <quantumerikfong@gmail.com>
2025-03-19 00:35:13 -0400

    Continued documentation efforts

--- src/types/sampling.rs
+++ src/types/sampling.rs
@@ -2,11 +2,23 @@ use strum::FromRepr;
 
 use crate::{sys::*, Error, Result};
 
+/// Describes the sampling modes/connection of the LMS6002D
+///
+/// This allows the user to switch between the "normal" sampling and direct samplling from the DACs and ADCs
+///
+/// Related `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/group___f_n___b_l_a_d_e_r_f1___s_a_m_p_l_i_n_g___m_u_x.html#gac8be10b9045b236e2bd4d705bde4b5db>
+// TODO Does this apply for the BladeRF 2.0 Micro?
 #[derive(Copy, Clone, Debug, FromRepr, PartialEq, Eq)]
 #[repr(i32)]
 pub enum Sampling {
+    /// Unable to determine the sampling type
     Unknown = bladerf_sampling_BLADERF_SAMPLING_UNKNOWN as i32,
+    /// Get samples from the RX/TX connector (ie: samples from the down/up-conversion to the RX/TX ports).
+    ///
+    /// This is the normal mode of operation for an SDR
     Internal = bladerf_sampling_BLADERF_SAMPLING_INTERNAL as i32,
+    /// Direct DAC/ADC sampling from the J60 and J61 headers
+    // TODO Does this apply for the BladeRF 2.0 Micro?
     External = bladerf_sampling_BLADERF_SAMPLING_EXTERNAL as i32,
 }
 
--- src/types/trigger.rs
+++ src/types/trigger.rs
@@ -4,13 +4,19 @@ use crate::{sys::*, Error, Result};
 
 use super::Channel;
 
-/// Trigger role
+/// Represents the role of a device in a trigger chain
+///
+/// Related `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/group___f_n___t_r_i_g.html#gae0ce25426c2eba648a28fa07a3436acc>
 #[derive(Copy, Clone, Debug, FromRepr, PartialEq, Eq)]
 #[repr(i32)]
 pub enum TriggerRole {
+    /// Invalid Role
     Invalid = bladerf_trigger_role_BLADERF_TRIGGER_ROLE_INVALID,
+    /// Triggering functionality is disabled on this device. Samples are not gated and the trigger signal is an input.
     Disabled = bladerf_trigger_role_BLADERF_TRIGGER_ROLE_DISABLED,
+    /// This device is the trigger master. Its trigger signal will be an output and this device will determine when all devices shall trigger.
     Master = bladerf_trigger_role_BLADERF_TRIGGER_ROLE_MASTER,
+    /// This device is the trigger slave. This device's trigger signal will be an input and this devices will wait for the master's trigger signal assertion.
     Slave = bladerf_trigger_role_BLADERF_TRIGGER_ROLE_SLAVE,
 }
 
@@ -24,20 +30,36 @@ impl TryFrom<bladerf_trigger_role> for TriggerRole {
 }
 
 /// Trigger signal selection
+///
+/// This selects pin or signal used for the trigger.
+///
+/// Related `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/group___f_n___t_r_i_g.html#gaebcb881ab6a5f975aaabfd87586f248d>
 #[derive(Copy, Clone, Debug, FromRepr, PartialEq, Eq)]
 #[repr(i32)]
 pub enum TriggerSignal {
+    /// Invalid selection
     Invalid = bladerf_trigger_signal_BLADERF_TRIGGER_INVALID,
+    /// J71 pin 4, mini_exp[1] on x40/x115
     J71_4 = bladerf_trigger_signal_BLADERF_TRIGGER_J71_4,
+    /// J51 pin 1, mini_exp[1] on xA4/xA5/xA9
     J51_1 = bladerf_trigger_signal_BLADERF_TRIGGER_J51_1,
+    /// mini_exp[1], hardware-independent
     MiniExp1 = bladerf_trigger_signal_BLADERF_TRIGGER_MINI_EXP_1,
+    /// Reserved for user SW/HW customizations
     User0 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_0,
+    /// Reserved for user SW/HW customizations
     User1 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_1,
+    /// Reserved for user SW/HW customizations
     User2 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_2,
+    /// Reserved for user SW/HW customizations
     User3 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_3,
+    /// Reserved for user SW/HW customizations
     User4 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_4,
+    /// Reserved for user SW/HW customizations
     User5 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_5,
+    /// Reserved for user SW/HW customizations
     User6 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_6,
+    /// Reserved for user SW/HW customizations
     User7 = bladerf_trigger_signal_BLADERF_TRIGGER_USER_7,
 }
 
@@ -51,10 +73,16 @@ impl TryFrom<bladerf_trigger_signal> for TriggerSignal {
 }
 
 /// Trigger configuration
+///
+/// Related `libbladerf` docs: <https://www.nuand.com/libbladeRF-doc/v2.5.0/structbladerf__trigger.html>
 pub struct Trigger {
+    /// The channel associated with this trigger
     pub channel: Channel,
+    /// The role of the device in this trigger chain
     pub role: TriggerRole,
+    /// The pin/signal being used
     pub signal: TriggerSignal,
+    /// Reserved for future use, should be set to zero.
     pub options: u64,
 }
 
