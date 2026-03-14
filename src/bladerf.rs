0e617845bbe26d7945e4a1e238894863c49e8e51
QuantumEF <quantumerikfong@gmail.com>
2026-03-14 17:47:11 -0400

    Hotfix since I realized there could be an issue with choosing relaxed as the default. (#47)

--- src/bladerf.rs
+++ src/bladerf.rs
@@ -127,7 +127,7 @@ impl BladeRfAny {
     ) -> Result<TxSyncStream<&Self, T, Self>> {
         // TODO: Decide Ordering
         self.tx_stream_configured
-            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
             .map_err(|_err| {
                 Error::Msg("Already have an TX stream open".to_owned().into_boxed_str())
             })?;
@@ -144,7 +144,7 @@ impl BladeRfAny {
         // TODO: Decide Ordering
         device
             .tx_stream_configured
-            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
             .map_err(|_err| {
                 Error::Msg("Already have an TX stream open".to_owned().into_boxed_str())
             })?;
@@ -160,7 +160,7 @@ impl BladeRfAny {
     ) -> Result<RxSyncStream<&Self, T, BladeRfAny>> {
         // TODO: Decide Ordering
         self.rx_stream_configured
-            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
             .map_err(|_err| {
                 Error::Msg("Already have an RX stream open".to_owned().into_boxed_str())
             })?;
@@ -177,7 +177,7 @@ impl BladeRfAny {
         // TODO: Decide Ordering
         device
             .rx_stream_configured
-            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
             .map_err(|_err| {
                 Error::Msg("Already have an RX stream open".to_owned().into_boxed_str())
             })?;
