bad35e48951a621aacd1d5cc340d340ea6f14877
Troy Neubauer <troyneubauer@gmail.com>
2026-03-16 20:50:26 -0700

    CI

--- bladerf.nix
+++ bladerf.nix
@@ -45,6 +45,8 @@ rec {
       "-DUDEV_RULES_PATH=etc/udev/rules.d"
       "-DINSTALL_UDEV_RULES=ON"
       "-DBLADERF_GROUP=bladerf"
+    ] ++ lib.optionals stdenv.hostPlatform.isLinux [
+      "-DCMAKE_C_FLAGS=-Wno-error=maybe-uninitialized"
     ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
       "-DCMAKE_C_FLAGS=-Wno-error=format"
     ];
